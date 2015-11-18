ff27c062f5123fff30430980e52c13e5887e807f
fuchu.ruan <fuchu.ruan@mokun.net>
2015-11-18 15:47:54 +0800

    add document folder and schedule file

--- program/src/engine/util/timer.h
+++ program/src/engine/util/timer.h
@@ -1,2 +1,20 @@
 #ifndef ENGINE_UTIL_TIMER_H
+#define ENGINE_UTIL_TIMER_H
+
+#if win32
+#include "time.h"
+#endif
+
+#include "../engine_pch.h"
+#include "singleton.h"
+
+namespace engine_util {
+	class Timer : public Singleton<Timer> {
+	public:
+		double GetCurrentTime() { return (double)clock(); }
+	protected:
+	private:
+	};
+}
+
 #endif
